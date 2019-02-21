dada341923be8d47755d3785907f44cc42278397
Nikos Tsourakis <Nikolaos.Tsourakis@unige.ch>
2019-02-22 00:47:17 +0100

    Procfile

--- poetry/dante/python/lite_call_runtime_top.py
+++ poetry/dante/python/lite_call_runtime_top.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+import sys
+sys.path.append('./dante/python/')
+sys.path.append('/app/poetry/python/')
+
 import lite_call_runtime as call_main
 #import sys
 #sys.path.append('C:/Projects/django/callector/api/python/')
